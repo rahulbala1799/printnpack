5456e34f433de86f194be3809148f89f604d3bc5
Rahul <rahul@printnpack.ie>
2025-03-28 17:16:17 +0000

    Fix package.json and next.config.js for local development

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    unoptimized: true,
-  }
+    domains: [],
+  },
 }
 
 module.exports = nextConfig 
