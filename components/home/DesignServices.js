27b6d8eb31ab45bbe752f909b983a8751065d1ba
Rahul <rahul@printnpack.ie>
2025-04-01 02:46:03 +0100

    Update Design Services with proper image paths and add image display

--- components/home/DesignServices.js
+++ components/home/DesignServices.js
@@ -48,7 +48,7 @@ const DesignServices = () => {
       id: 'posters',
       title: 'Posters',
       description: 'Eye-catching posters that demand attention and convey your message with impact.',
-      image: '/images/ifa/heroh/posters.png',
+      image: '/images/ifa/heroh/wide.png',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
@@ -61,7 +61,7 @@ const DesignServices = () => {
       id: 'vinyls',
       title: 'Vinyls',
       description: 'Premium vinyl stickers and graphics for branding that sticks with your customers.',
-      image: '/images/ifa/heroh/vinyls.png',
+      image: '/images/ifa/heroh/9.png',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
@@ -75,7 +75,7 @@ const DesignServices = () => {
       id: 'leaflets',
       title: 'Leaflets',
       description: 'Professional leaflets that put your information directly into customers\' hands.',
-      image: '/images/ifa/heroh/leaflets.png',
+      image: '/images/ifa/heroh/leaflet.png',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
@@ -88,7 +88,7 @@ const DesignServices = () => {
       id: 'menus',
       title: 'Menus',
       description: 'Appetizing menu designs that showcase your offerings and enhance the dining experience.',
-      image: '/images/ifa/heroh/menus.png',
+      image: '/images/ifa/heroh/5.png',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
@@ -203,6 +203,17 @@ const DesignServices = () => {
                     </div>
                     <h3 className="text-xl font-bold text-white mb-1">{service.title}</h3>
                     <div className="w-10 h-1 bg-white/50 rounded-full mb-2"></div>
+                  </div>
+                  
+                  {/* Service image */}
+                  <div className="absolute right-2 bottom-2 w-24 h-24 overflow-hidden">
+                    <Image
+                      src={service.image}
+                      alt={service.title}
+                      width={96}
+                      height={96}
+                      className="object-contain drop-shadow-md transform group-hover:scale-110 transition-transform duration-300"
+                    />
                   </div>
                   
                   {/* Floating design elements */}
