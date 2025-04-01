f3e9703e6b912b5d0036181da729a83dd6ed4842
Rahul <rahul@printnpack.ie>
2025-04-01 10:30:36 +0100

    Fix: Corrected ServiceIcon import path in DesignServices component

--- components/home/DesignServices.js
+++ components/home/DesignServices.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
-import ServiceIcon from '../ServiceIcon';
+import ServiceIcon from '../../components/ServiceIcon';
 
 const DesignServices = () => {
   const [activeService, setActiveService] = useState(null);
